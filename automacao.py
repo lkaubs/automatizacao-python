09a9b79d758c88a09664a71ebe4fcbf7679f7a2d
lkaubs <kauanmessi@hotmail.com>
2024-09-03 17:13:31 -0300

    Commit Automacao

---
+++ automacao.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# Inicializar o WebDriver (usando ChromeDriver como exemplo)
+driver = webdriver.Chrome()
+driver.get('https://www.google.com')
